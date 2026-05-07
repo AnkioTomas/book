ddf65b957bfce6104f0ea43489e9106575d4f216
ankio <ankio@ankio.net>
2026-05-07 15:58:26 +0800

    :bookmark: (core): update version to 1.0.2
    更新项目版本号至 1.0.2

--- src/example.config.php
+++ src/example.config.php
@@ -15,7 +15,7 @@ return [
     'timezone'     => 'Asia/Shanghai',
     'default_route' => true,
     'domain'       => ['0.0.0.0'],
-    'version'      => '1.0.0',
+    'version'      => '1.0.2',
 
     'framework_start' => [
         'nova\\plugin\\login\\LoginManager',
