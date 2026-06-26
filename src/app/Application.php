8025bfbad3b1d97c9e58bc44cc7fa22758b60a01
ankio <ankio@ankio.net>
2026-06-26 22:32:04 +0800

    refactor(core): 优化路由和任务管理逻辑

--- src/app/Application.php
+++ src/app/Application.php
@@ -12,53 +12,33 @@ declare(strict_types=1);
 
 namespace app;
 
+use app\task\SyncTask;
 use app\utils\Installer;
 use nova\framework\App;
+use nova\framework\event\EventManager;
 
 use function nova\framework\route;
 
 use nova\framework\route\Route;
+use nova\plugin\corn\schedule\TaskerManager;
 
-use nova\plugin\login\LoginTpl;
-use nova\plugin\task\PoolManager;
+use nova\plugin\corn\schedule\TaskerTime;
 
 class Application extends App
 {
     public function onFrameworkStart(): void
     {
         Installer::register();
-        PoolManager::start();
-        LoginTpl::getInstance()->registerRouter('index', 'main');
-        // Route::getInstance()->get()
+
+        $adminRoute = ['index', 'main'];
+        EventManager::trigger('admin.router', $adminRoute);
+
         Route::getInstance()
             ->get("/", route('index', 'main', 'index'))
-            ->get("/admin/dashboard", route('index', 'main', 'dashboard'))
-            ->get("/admin/book", route('index', 'main', 'book'))
-            ->get("/admin/reader", route('index', 'book', 'reader'))
-            ->get("/admin/webdav", route('index', 'main', 'webdav'))
-            ->get("/admin/task", route('index', 'main', 'task'))
-            ->getOrPost('/admin/api/book/list', route('index', 'book', 'list'))
-            ->getOrPost('/admin/api/book/filters', route('index', 'book', 'filters'))
-            ->get('/admin/api/book/file', route('index', 'book', 'file'))
-            ->get('/admin/api/book/progress', route('index', 'book', 'progress'))
-            ->post('/admin/api/book/progressSync', route('index', 'book', 'progressSync'))
-            ->post('/admin/api/book/progressUpdate', route('index', 'book', 'progressUpdate'))
-            ->post('/admin/api/book/update', route('index', 'book', 'update'))
-            ->post('/admin/api/book/batchRead', route('index', 'book', 'batchRead'))
-            ->post('/admin/api/book/delete', route('index', 'book', 'delete'))
-            ->post('/admin/api/sync', route('index', 'book', 'sync'))
-            ->post("/admin/api/book/removeDuplicates", route('index', 'book', 'removeDuplicates'))
-            ->post('/admin/api/book/scrapeCover', route('index', 'book', 'scrapeCover'))
-            //filters
-            ->getOrPost('/admin/api/webdav', route('index', 'webdav', 'config'))
-            ->get('/admin/calibre', route('index', 'main', 'calibre'))
-            ->getOrPost('/admin/api/calibre', route('index', 'calibre', 'config'))
-            ->post('/admin/api/calibre/test', route('index', 'calibre', 'test'))
-            ->post("/admin/api/upload", route("index", "upload", "upload")) // 文件上传
-            ->post("/admin/api/publish", route("index", "upload", "publish")) // 文件上传
-            ->post("/admin/api/douban", route("index", "douban", "search"))
             ->get("/webdav/{filename}", route('index', 'douban', 'webdav'))
             ->get("/proxy/{uri}", route('index', 'douban', 'proxy'));
+
+        TaskerManager::add(TaskerTime::hour(1), new SyncTask(), 'sync_books', -1);
 
     }
 
